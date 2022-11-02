0bb8e59ec9fd22d731fb94a64f287aae3512fbbf
CHETNAPATIL07 <117268497+CHETNAPATIL07@users.noreply.github.com>
2022-11-02 15:07:13 +0530

    Add files via upload

---
+++ basic3.h
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JavaScript</title>
+</head>
+<body>
+<script type="text/javascript">
+	var age1 = parseInt(prompt("Enter First No."));
+	var age2 = parseInt(prompt("Enter Second No."));
+	var result = age1 + age2;
+	alert(result);
+	document.write("<h1>The Addition is"+result+"<h1>"); 
+	if(Result>=20); {
+		"you are eligible for voting";
+	} else {
+		"you are not eligible";
+	}
+
+
+
+</script>
+
+</body>
+</html>
